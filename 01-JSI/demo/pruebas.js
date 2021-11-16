1294c340e699cbcb9a8b8b72135df8886abef48a
Linux <octavio2301@gmail.co>
2021-11-15 19:44:18 -0800

    Demo HTML

---
+++ 01-JSI/demo/pruebas.js
@@ -0,0 +1,17 @@
+function tienenMismaLongitud(str1, str2) {
+    // Devuelve "true" si las dos strings tienen la misma longitud
+    // De lo contrario, devuelve "false"
+    // Tu código:
+  str1= "x";
+  str2= "y";
+  if(x==y) {
+    console.log("Tienen los mismos caracteres");
+    return true;
+    
+  
+  } else if (x!==y)
+  console.log("No tiene los mismos caracteres");
+  return false;
+  
+  }
+console.log(str1==str2)
--- 01-JSI/homework/homework.js
+++ 01-JSI/homework/homework.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta correcta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "Octavio";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 8;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === null;
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+return(x-y);
 
 }
 
@@ -35,20 +36,45 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(x===y) {
+    console.log("Son Iguales");
+    return true;
+    
+
+  } else if (x!==y)
+  console.log("No son Iguales");
+  return false;
 
 }
 
-function tienenMismaLongitud(str1, str2) {
-  // Devuelve "true" si las dos strings tienen la misma longitud
-  // De lo contrario, devuelve "false"
-  // Tu código:
 
-}
+  function tienenMismaLongitud(str1, str2) {
+    // Devuelve "true" si las dos strings tienen la misma longitud
+    // De lo contrario, devuelve "false"
+    // Tu código:
+
+    if(str1.length === str2.length){
+      return true;
+  }else {
+    
+    return false;
+  }
+      
+  
+  
+  }
+
+
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num<90){
+    return true;
+
+  }else (num>=90)
+  return false;
 
 }
 
@@ -56,6 +82,10 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+if(num % 2 === 1 ) {
+return true;
+}else 
+  return false;
 
 }
 
@@ -63,13 +93,13 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // Ojo: No es raiz cuadrada!
   // Tu código:
-
+return num * num;
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-
+return num ** exponent;
 }
 
 function esPositivo(numero) {
@@ -77,6 +107,16 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+if (numero > 0){
+  return "Es positivo";
+
+} else if (numero < 0) {
+  return "Es negativo";
+
+}else {
+  return false;
+}
+     
 
 }
 
@@ -84,12 +124,14 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  return nombre + ' ' +apellido;
 
 }
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   // Tu código:
+  return 4 * lado;
 
 }
 
@@ -97,7 +139,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   // Tu código:
-  
+  return euro * 1.20
 }
 
 
@@ -106,7 +148,14 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Tu código:
+ if(letra.length > 1) {
+  return "Dato incorrecto";
+ }
+  if(letra === "a" || letra === "e" || letra=== "i" || letra=== "o"|| letra==="u") {
+    return "Es vocal";
+  }
 
+ 
 }
 
 function obtenerMayor(x, y) {
@@ -114,7 +163,13 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+if(x > y) {
+  return x;
 
+}else {
+  return y;
+  
+}
 }
 
 /*
